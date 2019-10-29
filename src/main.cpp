ba9de23502903f357ad3f3983b53b3d1c42fb01d
dakotadvanced <46972988+dakotadvanced@users.noreply.github.com>
2019-10-29 14:14:33 -0500

    okapi is now enabled through the removed commenting

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,11 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
+ okapi::ControllerButton intakeButton = okapi::ControllerDigital::R2;
+ okapi::ControllerButton trayButton = okapi::ControllerDigital::L1;
+ okapi::ControllerButton outtakeButton = okapi::ControllerDigital::R1;
+ okapi::ControllerButton liftButton = okapi::ControllerDigital::L2;
+ okapi::ControllerButton	driverDan = okapi::ControllerDigital::B;
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
