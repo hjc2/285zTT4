10f7aac88b4d02cac9eb2fef6a38040a84308609
hcolemantx <46972988+dakotadvanced@users.noreply.github.com>
2019-11-06 09:42:11 -0600

    tried lift recirculation within the class?

--- src/285Z_Subsystems/lift.cpp
+++ src/285Z_Subsystems/lift.cpp
@@ -3,9 +3,13 @@ public:
 
   double liftAngle;
   bool liftState;
-
   void liftPosition(int position){
     liftMotor.moveAbsolute(position);
+    
   }
 
 };
+
+lift myLift(0){
+}
+myLift.liftAngle
