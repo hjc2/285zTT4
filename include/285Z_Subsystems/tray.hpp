6429ec703b6c3df74596680084a81acc84cb49e1
Ekin Tiu <ekintiu@gmail.com>
2019-11-05 19:51:38 -0600

    tray

--- include/285Z_Subsystems/tray.hpp
+++ include/285Z_Subsystems/tray.hpp
@@ -1,1 +1,3 @@
 #include "main.h"
+
+class Tray;
--- src/285Z_Subsystems/tray.cpp
+++ src/285Z_Subsystems/tray.cpp
@@ -1,7 +1,29 @@
 #include "../include/285z/initRobot.hpp"
 
 class Tray {
-  Tray(){
 
+  const int UP = true;
+  const bool DOWN = false;
+
+  //Tray Class Attributes
+  public:
+    bool pos;
+
+  // Tray Constructor
+  Tray(int initPos){
+    pos = initPos;
   }
+
+  //Tray Functions
+  void moveToState(bool pos){
+    switch(pos){
+      case true:
+        //UP
+      case false:
+        //DOWN
+        
+      break;
+    }
+  }
+
 };
