dd50a61907e1347bd588862171d13f41b624e184
Ekin Tiu <ekintiu@gmail.com>
2019-11-15 14:55:28 -0600

    red long goal

--- src/285Z_Main/autFunctions.cpp
+++ src/285Z_Main/autFunctions.cpp
@@ -122,41 +122,6 @@ void redLongGoal(std::shared_ptr<okapi::OdomChassisController> chassis){
   intake.moveVelocity(270);
   //Move to blocks
   chassis->setState({0.5_ft,9.9_ft,0_deg});
-  chassis->driveToPoint({5.05_ft, 9.9_ft});
-  pros::Task::delay(500);
-//  chassis->turnToAngle(135_deg);
-  intake.setBrakeMode(AbstractMotor::brakeMode::hold);
-  intake.moveVelocity(0);
-
-  chassis->driveToPoint({2.5_ft, 9.9_ft}, true);
-  chassis->waitUntilSettled();
-	chassis->turnToAngle(160_deg);
-  chassis->waitUntilSettled();
-
-
-  chassis->setState({0.5_ft,9.9_ft,0_deg});
-  intake.moveRelative(-725, 30);
-  chassis->driveToPoint({2_ft, 9.9_ft});
-
-  angler.moveToUp(false);
-  //false means its auton
-  //intake.moveRelative(-700, 50);
-
-  pros::Task::delay(2300);
-  intake.moveRelative(-300, 110);
-  pros::Task::delay(200);
-  chassis->moveDistance(-1.5_ft);
-  angler.moveToDown(false);
-}
-
-void blueLongGoal(std::shared_ptr<okapi::OdomChassisController> chassis){
-  Tray angler;
-
-  //       RED AUTON       //
-  //Intake On
-  intake.moveVelocity(270);
-  //Move to blocks
-  chassis->setState({0.5_ft,9.9_ft,0_deg});
   chassis->driveToPoint({4.5_ft, 9.9_ft});
   pros::Task::delay(500);
 //  chassis->turnToAngle(135_deg);
@@ -164,13 +129,13 @@ void blueLongGoal(std::shared_ptr<okapi::OdomChassisController> chassis){
 
 
   chassis->driveToPoint({2.5_ft, 9.9_ft}, true);
-	chassis->turnToAngle(100_deg);
+	chassis->turnToAngle(-100_deg);
 
 
   chassis->setState({0.5_ft,9.9_ft,0_deg});
   chassis->driveToPoint({2.0_ft, 9.9_ft});
   intake.moveVelocity(0);
-  chassis->turnToAngle(45_deg);
+  chassis->turnToAngle(-45_deg);
   intake.moveRelative(-725, 30);
   chassis->setState({0.5_ft,9.9_ft,0_deg});
   chassis->driveToPoint({1.75_ft, 9.9_ft});
