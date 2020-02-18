a8daaf216c6942fac2861290c54eb290a8d64fbd
Ekin Tiu <ekintiu@gmail.com>
2020-02-17 23:25:17 -0600

    Create long goal draft auton for red

--- src/285Z_Main/redAutons.cpp
+++ src/285Z_Main/redAutons.cpp
@@ -8,7 +8,6 @@
 
 //*****************************   RED: FIVE CUBES    **********************//
 void sgSixRed(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_ptr<okapi::AsyncMotionProfileController> slow,std::shared_ptr<okapi::AsyncMotionProfileController> fast){
-  Tray angler;
   Lift lift;
   lift.deploy();
 
@@ -134,48 +133,76 @@ void sgNineRed(std::shared_ptr<okapi::OdomChassisController> chassis, std::share
 //************************   RED: LONG GOAL   ****************************//
 void lgRed(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_ptr<okapi::AsyncMotionProfileController> slow,std::shared_ptr<okapi::AsyncMotionProfileController> fast){
   Tray angler;
+  Lift lift;
+  lift.deploy();
 
+  intake.moveVelocity(200);
   //************** INIT PATHS *******************//
-  slow->generatePath({
+  fast->generatePath({
     {0_ft,0_ft,0_deg},
-    {4.5_ft,0_ft,0_deg}},
+    {1_ft,0_ft,0_deg}},
     "F1"
   );
 
   fast->generatePath({
     {0_ft,0_ft,0_deg},
-    {3.5_ft,0_ft,0_deg}},
+    {2_ft,0_ft,0_deg}},
+    "F2"
+  );
+
+  fast->generatePath({
+    {0_ft,0_ft,0_deg},
+    {1.75_ft,0_ft,0_deg}},
+    "F3"
+  );
+
+  fast->generatePath({
+    {0_ft,0_ft,0_deg},
+    {1.7_ft,0_ft,0_deg}},
     "B1"
   );
 
   fast->generatePath({
     {0_ft,0_ft,0_deg},
-    {4_ft,0_ft,0_deg}},
-    "F2"
+    {2_ft,0_ft,0_deg}},
+    "B2"
   );
+
   fast->generatePath({
     {0_ft,0_ft,0_deg},
-    {1.5_ft,0_ft,0_deg}},
+    {0.4_ft,0_ft,0_deg}},
+    "B3"
+  );
+
+  slow->generatePath({
+    {0_ft,0_ft,0_deg},
+    {0.5_ft,0_ft,0_deg}},
     "G"
   );
-  //************** RUN AUTON *******************//
-  slow->setTarget("F1", fwd);
-  slow->waitUntilSettled();//goes forward to get 2 cubes from VERTICAL Stack
 
-  chassis->turnToAngle(-35_deg); //intake cube in front of tower
-  chassis->turnToAngle(35_deg);
-  fast->setTarget("B1", bwd);
-  fast->waitUntilSettled();//move backwards straight
+  fast->setTarget("F1", fwd);
+  fast->waitUntilSettled();//goes forward to get 4 cubes
+  turn(30);
 
-  chassis->turnToAngle(-95_deg); //turn to cube next to long goal
-  slow->setTarget("F2", fwd);
-  slow->waitUntilSettled();//intakes one cube
-
-  chassis->turnToAngle(-50_deg);
-  fast->setTarget("G");//drives to goal zone
+  fast->setTarget("F2", fwd);
   fast->waitUntilSettled();
 
-  //deploy
-  intake.moveAbsolute(-90, 60);
+  fast->setTarget("B1", bwd);
+  fast->waitUntilSettled();
+  turn(95);
+
+  fast->setTarget("F3", bwd);
+  fast->waitUntilSettled();
+
+  fast->setTarget("B2", bwd);
+  fast->waitUntilSettled();
+  turn(220);
+
+  slow->setTarget("G");//drives to goal zone
+  slow->waitUntilSettled();
+
   autoStackDeploy();
+
+  fast->setTarget("B3");
+  fast->waitUntilSettled();
 }
