d7df8b9e34ad45a99473c8d2485d520435d4ef9a
Ekin Tiu <ekintiu@gmail.com>
2020-02-17 23:59:17 -0600

    Update skills draft and add comments

--- src/285Z_Main/auxAutons.cpp
+++ src/285Z_Main/auxAutons.cpp
@@ -29,6 +29,12 @@ void skills(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_p
     "F1"
   );
 
+  slow->generatePath({
+    {0_ft,0_ft,0_deg},
+    {1.85_ft,0_ft,0_deg}},
+    "F2"
+  );
+
   fast->generatePath({
     {0_ft,0_ft,0_deg},
     {0.5_ft,0_ft,0_deg}},
@@ -41,18 +47,24 @@ void skills(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_p
     "G"
   );
 
+  //PART 1: INTAKE 10 CUBES AND TURN TO GOAL
   slow->setTarget("F1", fwd);
   slow->waitUntilSettled();//goes forward to get 4 cubes
   turn(45);
   intake.moveVelocity(0);
 
+  //PART 2: MOVE TO GOAL, DEPLOY, BACK UP, TURN TO TOWER CUBE
   slow->setTarget("G", fwd);
   slow->waitUntilSettled();
 
   autoStackDeploy();
 
-  fast->setTarget("B1");
+  fast->setTarget("B1", bwd);
   fast->waitUntilSettled();
+  turn(270);
+
+  //PART 3: INTAKE NEXT BATCH OF CUBES
+
 }
 
 //****************** ONE CUBE ***********************************//
