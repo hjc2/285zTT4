89cb6c19427ad0b841829db385e05cd64cee510a
Ekin Tiu <ekintiu@gmail.com>
2020-02-17 23:54:56 -0600

    Create skills draft 10 cube

--- src/285Z_Main/auxAutons.cpp
+++ src/285Z_Main/auxAutons.cpp
@@ -17,7 +17,42 @@ void autoStackDeploy() {
 
 //****************** ONE CUBE ***********************************//
 void skills(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_ptr<okapi::AsyncMotionProfileController> slow,std::shared_ptr<okapi::AsyncMotionProfileController> fast){
+  Tray angler;
+  Lift lift;
+  lift.deploy();
 
+  intake.moveVelocity(200);
+  //************** INIT PATHS *******************//
+  slow->generatePath({
+    {0_ft,0_ft,0_deg},
+    {6_ft,0_ft,0_deg}},
+    "F1"
+  );
+
+  fast->generatePath({
+    {0_ft,0_ft,0_deg},
+    {0.5_ft,0_ft,0_deg}},
+    "B1"
+  );
+
+  slow->generatePath({
+    {0_ft,0_ft,0_deg},
+    {0.9_ft,0_ft,0_deg}},
+    "G"
+  );
+
+  slow->setTarget("F1", fwd);
+  slow->waitUntilSettled();//goes forward to get 4 cubes
+  turn(45);
+  intake.moveVelocity(0);
+
+  slow->setTarget("G", fwd);
+  slow->waitUntilSettled();
+
+  autoStackDeploy();
+
+  fast->setTarget("B1");
+  fast->waitUntilSettled();
 }
 
 //****************** ONE CUBE ***********************************//
