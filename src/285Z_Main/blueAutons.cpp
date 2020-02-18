5c01aeaa5f13a9b1bd5f795ce0e0927d441697c8
Ekin Tiu <ekintiu@gmail.com>
2020-02-18 13:49:33 -0600

    Create blue nine cube draft with move

--- src/285Z_Main/blueAutons.cpp
+++ src/285Z_Main/blueAutons.cpp
@@ -34,60 +34,31 @@ void sgSixBlue(std::shared_ptr<okapi::OdomChassisController> chassis, std::share
 //************************   BLUE: SHORT GOAL, NINE CUBES   ****************************//
 void sgNineBlue(std::shared_ptr<okapi::OdomChassisController> chassis, std::shared_ptr<okapi::AsyncMotionProfileController> slow,std::shared_ptr<okapi::AsyncMotionProfileController> fast){
   Tray angler;
-  Lift theLift;
+  Lift lift;
 
-  //************** INIT PATHS *******************//
-  slow->generatePath({
-    {0_ft,0_ft,0_deg},
-    {4_ft,0_ft,0_deg}},
-    "F1"
-  );
-
-  fast->generatePath({
-    {0_ft,0_ft,0_deg},
-    {3_ft,0_ft,0_deg}},
-    "B1"
-  );
-  slow->generatePath({
-    {0_ft,0_ft,0_deg},
-    {3.75_ft,0_ft,0_deg}},
-    "F2"
-  );
-
-  fast->generatePath({
-    {0_ft,0_ft,0_deg},
-    {5_ft,0_ft,0_deg}},
-    "F3"
-  );
-
-  slow->generatePath({
-    {0_ft,0_ft,0_deg},
-    {0.5_ft,0_ft,0_deg}},
-    "B2"
-  );
-  theLift.deploy();
+  lift.deploy();
 
   intake.moveVelocity(200);
 
-  slow->setTarget("F1", fwd);
-  slow->waitUntilSettled();//goes forward to get 4 cubes
+  move(fast, 3.20_ft, fwd);
   turn(30);
 
-  fast->setTarget("B1", bwd);
-  fast->waitUntilSettled();//moves backward to align with other 4 cubes
+  move(fast, 3.20_ft, bwd);
   turn(0);
 
-  slow->setTarget("F2", fwd);
-  slow->waitUntilSettled();//intakes last 4 cubes
-  turn(210);
+  move(fast, 3.1_ft, fwd);
 
-  fast->setTarget("F3", fwd);//drives to goal zone
-  fast->waitUntilSettled();
+  move(fast, 2_ft, bwd);
+  intake.moveVelocity(0);
+  turn(230);
+
+  move(slow, 1.3_ft, fwd);
 
   autoStackDeploy();
 
-  slow->setTarget("B2", bwd);
-  slow->waitUntilSettled();
+  intake.moveVelocity(-120);
+  move(fast, 1_ft, bwd);
+  angler.moveToDown(false);
 }
 
 
